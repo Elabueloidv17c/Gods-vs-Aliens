58610828c707c400dbacdc1fc7137f317719b88e
Andres Gonzalez <andres.gonconde@gmail.com>
2019-01-23 16:29:39 -0600

    Added more Parallax code
    Se agrego codigo para el Parallax despues de hacer pruebas

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Depth
+{
+    Background_1,
+    Background_2,
+    Background_3,
+    Background_4,
+    PlayArea_1,
+    PlayArea_2,
+    PlayArea_3,
+    ForwardGround_1,
+    ForwardGround_2,
+    ForwardGrond_3,
+    ForwardGround_4
+}
+
 public class Parallax : MonoBehaviour
 {
     public Transform[] backgrounds;     //Arreglo de los backgrounds para el parallax
@@ -24,9 +39,10 @@ public class Parallax : MonoBehaviour
 
         //Asignar las escalas de losprallax correspondientes
         parallaxScales = new float[backgrounds.Length];
-        for(int i=0; i<backgrounds.Length; i++)
+        for (int i = 0; i < backgrounds.Length; i++)
         {
             parallaxScales[i] = backgrounds[i].position.z * -1;
+            Debug.Log(parallaxScales[i]);
         }
     }
 
@@ -34,11 +50,11 @@ public class Parallax : MonoBehaviour
     void Update()
     {
         //para cada background
-        for (int i = 0; i<backgrounds.Length; i++)
+        for (int i = 0; i < backgrounds.Length; i++)
         {
+
             //El parallax es el opuesto del movimiento de la camara debido al frame oasado multiplicado por la escala
             float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
-
             //Acomoda un objetivo posixion x la cual es la posicion actual mas el parallax
             float backgroundTargetPosX = backgrounds[i].position.x + parallax;
 
